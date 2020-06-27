092268c2ef014bca963937832e0de0eb13251e67
= <=>
2020-06-27 02:48:55 +0000

    Adds error handling

--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -1,7 +1,12 @@
 class ParseController < ApplicationController
  def create
   result = `python3 parselog.py --json #{params[:id].to_i}`
-  render json: result
+  
+  if result[0..4] == "Error"
+    render json: { error: "Can't parse file", log: [{name: "Error - can't parse file", v: result}] }
+  else
+    render json: result
+  end
  end
  
  def ping
